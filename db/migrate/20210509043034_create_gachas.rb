fe81a9e01d80229f08a46e220c7b0b8931884ab2
kinoppyd <WhoIsDissolvedGirl+github@gmail.com>
2021-05-09 13:59:28 +0900

    Create gacha table

--- db/migrate/20210509043034_create_gachas.rb
+++ db/migrate/20210509043034_create_gachas.rb
@@ -1,7 +1,9 @@
 class CreateGachas < ActiveRecord::Migration[6.1]
   def change
-    create_table :gachas do |t|
-
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    create_table :gachas, id: :uuid do |t|
+      t.string "items", array: true
+      t.string "seed"
       t.timestamps
     end
   end
