5059bdf9287dec9e9ba4f301f57f81dc9522443b
kinoppyd <ppyd@kinoppyd.dev>
2023-05-05 00:12:43 +0900

    Fix for sqlite

---
+++ app/models/concerns/uuid_primary_key.rb
@@ -0,0 +1,12 @@
+module UuidPrimaryKey
+  def self.included(klass)
+    klass.before_create :generate_id
+  end
+
+  def generate_id
+    self.id = loop do
+      uuid = SecureRandom.uuid
+      break uuid unless self.class.exists?(id: uuid)
+    end
+  end
+end
--- app/models/gacha.rb
+++ app/models/gacha.rb
@@ -4,10 +4,21 @@
 # Gacha
 #
 class Gacha < ApplicationRecord
+  include UuidPrimaryKey
+
+  serialize :items, ActiveRecord::Coders::JSON
+
   validates :items, presence: true
+  validate :items_not_empty
   validates :seed, presence: true, numericality: { only_integer: true }
 
   def gacha
     items.shuffle(random: Random.new(seed.to_i))
+  end
+
+  private
+
+  def items_not_empty
+    errors.add(:items, 'Items must not be blank') if items.blank?
   end
 end
